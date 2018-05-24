5c6295d5caa56019b64b990e65ef0f213e98b1da
five.liu <five.liu@harmontronics.com>
2018-05-24 17:42:14 +0800

    图片

--- src/components/shop/ShopBasic.vue
+++ src/components/shop/ShopBasic.vue
@@ -84,7 +84,8 @@
                   :onSuccess="uploadSuccess"
                   :beforeUpload="beforeAvatarUpload"
                   :file-list="fileList2"
-                  list-type="picture">
+                  list-type="picture"
+                  limit="3">
                   <el-button size="small" type="primary">点击上传</el-button>
                 <!--  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>-->
                 </el-upload>
